ec8808dbbd6821222b3ec12a0c46e8e71eeb1d07
Juan David Piza <piza.juan1@gmail.com>
2017-01-20 14:18:38 -0400

    Nav tabs proyectos y datasets
    Set arregló las rutas de proyectos y datasets y tambien se le agregaron
    algunos estilos.

--- client/partials/NavProjectDatasets.js
+++ client/partials/NavProjectDatasets.js
@@ -1,3 +1,16 @@
 Template.NavProjectDatasets.onCreated(function() {
     console.log('creada nav dave');
 });
+
+Template.NavProjectDatasets.events({
+  'click #projectsTab'(event, template) {
+    event.preventDefault();
+    console.log('to projects');
+    FlowRouter.go('proyectos');
+  },
+  'click #datasetsTab'(event, template) {
+    event.preventDefault();
+    console.log('to datasets');
+    FlowRouter.go('datasets');
+  },
+});
