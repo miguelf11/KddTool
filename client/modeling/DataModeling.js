ddd98848ade14b852935b80204206e65ae049dd7
Dave Padrino <david.padrino3@hotmail.com>
2017-04-19 18:19:00 -0400

    fix jquery validator and adding comments, disabling submit button

--- client/modeling/DataModeling.js
+++ client/modeling/DataModeling.js
@@ -152,56 +152,57 @@ Template.DataModeling.helpers({
 });
 
 Template.DataModeling.events({
+
+    /* function to call R and execute function stack */
     'click .execute': function(e) {
         var numberOfParentChildren = $('.ui-droppable .dropped').length;
-        var arrayLindo = []
-        console.log(numberOfParentChildren);
+        var arrayOfParams = []
         var i = 0;
         $('.ui-droppable .dropped').each(function() {
             var data = $(this).data('node'); 
-            arrayLindo[i] = data;
+            arrayOfParams[i] = data;
             i++;
         });
-        console.log(arrayLindo);
-        Meteor.call('example10', arrayLindo, function(error, result){
+        Meteor.call('example10', arrayOfParams, function(error, result){
             if(error){
                 console.log(error);
             } else {
-                // console.log(result);
-                //out = JSON.stringify(result);
                 out = result;
-                console.log("salidaaaaaaaaaaaa,  "+JSON.stringify(result));
-                console.log("salidaaaaaaaaaaaa,  "+result);
             }
         });
     },
 
+    /* Delete nodes */
     'click #drop-node': function(e) {
         // $(e.currentTarget).parent().nextAll('.ui-nodeEditor-wire').remove();
         var numberOfParentChildren = $(e.currentTarget).closest('.ui-droppable').children().length;
         var numberOfConnections = $(e.currentTarget).parent().children().length-2;
         var numberOfCurrentBox = $(e.currentTarget).parent().index()+1;
         var cont = 0;
-        console.log("# Parent children: "+numberOfParentChildren);
-        console.log("# of my connections: "+numberOfConnections);
-        console.log("current position Box: "+numberOfCurrentBox);
-        console.log("############################");
+
+        /* Delete all wires mades after deleted node, FIX */
         while (cont != numberOfConnections) {
-            console.log("DENTRO DEL WHILE");
             $(e.currentTarget).parent().nextAll('.ui-nodeEditor-wire:first').remove();
             cont++; 
         }
         $(e.currentTarget).parent().fadeOut(300, function() {
             $(this).remove();
         });
-        //console.log("NextAll: "+Object.keys($(e.currentTarget).nextAll('.ui-nodeEditor-wire')));
+
+        /* Remove text from attrs table */
         if ($('#Aparams').text() == $(e.currentTarget).parent().data('node').label || !$('#Aparams').text()) {
             $('#Aparams').text("");
             $('.new-elements').remove();
         }
+
+        /* Disable executor prevent R Serve calls */
+        if ( $('.ui-nodeEditor-wire').length == 0) {
+            $('.execute').attr("disabled", true);
+        }
         e.stopPropagation();
     },
 
+    /* Activate nodes' attributes table */
     'click .ui-nodeEditor-Node': function(e) {
 
         // defining global for accesing from the next event
@@ -212,16 +213,12 @@ Template.DataModeling.events({
         var parametros = currentNode.data('node').parametros;
         var labelOutput = currentNode.data('node').outputs[0].label;
 
-
-        //var attrsInput = $(e.currentTarget).data('node').inputs[0].attrs;
         $('.new-elements').remove();
         $('#Aparams').text(label);
 
-
-
         if(parametros[0]) {
             $(".attrs-table")
-                .append("<tr class='new-elements'><td><form class= 'form' action='' onsubmit='event.preventDefault();'></form></td></tr>");
+                .append("<tr class='new-elements'><td><form id='form' class= 'form' action='' onsubmit='event.preventDefault();'></form></td></tr>");
             for(var i in parametros) {   
                 name = parametros[i].key;
                 value = parametros[i].value;
@@ -249,14 +246,71 @@ Template.DataModeling.events({
                         .append("<tr class='new-elements'><td><label for="+name+">"+name+"</label><br>"+input+"</td></tr>");
                 }
             }
-            $(".form").append("<br><input id='submit' type='submit' class='btn btn-primary' value='Actualizar parámetros'>");
+            $(".form").append("<br><input id='submit' type='submit' name='submit' class='btn btn-primary' value='Actualizar parámetros'>");
+            $(".form").append("<br><label id='submit-success' for='submit'>Parámetros actualizados correctamente!</label>");
         } else {
             $(".attrs-table")
                 .append("<tr class='new-elements'><td>No hay parámetros para este elemento</td></tr>");
-
         }
+
+        /* jQuery form validator */
+        $('#form').validate({
+            submitHandler: function (form) {
+                $('#submit-error').hide();
+                $('#submit-success').show();
+                form.submit();
+            },
+            invalidHandler: function(event, validator) {
+                // 'this' refers to the form
+                console.log("INCORRECTO");
+                var errors = validator.numberOfInvalids();
+                if (errors) {
+                    var message = errors == 1
+                        ? '<br><label id="submit-error" for="submit">Parámetros no actualizados, existe un error en algun campo</label>'
+                        : '<br><label id="submit-error" for="submit">Parámetros no actualizados, existen '+errors+' errores en el formulario</label>';
+                    $(".form").append(message);
+                    console.log("message :"+errors);
+                    $('#submit-success').hide();
+                    $('#submit-error').show();
+                } else {
+                    $('#submit-error').hide();
+                }
+            },
+
+            rules: {
+                number: {
+                    required: true,
+                    minlength: 1,
+                    number: true,
+                },
+                text: {
+                    required: true,
+                    minlength: 1,
+                    lettersonly: true
+                },
+            },
+
+            messages: {
+                number: {
+                    required: "Este campo es requerido",
+                    minlength: "Introduzca al menos un número",
+                    number: "El valor debe ser de tipo numérico",
+                },
+                text: {
+                    required: "Este campo es requerido",
+                    minlength: "Introduzca al menos un caracter",
+                    lettersonly: "El valor debe ser de tipo NO numérico",
+                },
+            },
+        });
+
+        jQuery.validator.addMethod("lettersonly", function(value, element) {
+            return this.optional(element) || /^[a-z]+$/i.test(value);
+        }, "Letters only please"); 
+
     },
 
+    /* submit form function */
     'click #submit': function(e) {
         var i = 0 ;
         var parametros = currentNode.data('node').parametros;
@@ -283,36 +337,9 @@ Template.DataModeling.events({
         currentNode.data('node',node);
     },
 
+
 });
 
-// $(document).ready(function() {
-//     $('.form').validate({
-//         rules: {
-//             number: {
-//                 required: true,
-//                 minlength: 1,
-//                 number: true,
-//             },
-//             text: {
-//                 required: true,
-//                 minlength: 1,
-//                 text: true,
-//             },
-//         },
-//         messages: {
-//             number: {
-//                 required: "Este campo es requerido",
-//                 minlength: "Introduzca al menos un número",
-//                 number: "El valor debe ser de tipo numérico",
-//             },
-//             text: {
-//                 required: "Este campo es requerido",
-//                 minlength: "Introduzca al menos un caracter",
-//                 text: "El valor debe ser de tipo texto",
-//             },
-//         }
-//     });
-// });
 // Luego de realizar cualquier tarea en esta etapa se debe modificar el stage 
 // de la coleccion projecto y se debe colocar 'modelado' para que al volver a 
 // ingresar al proyecto lo redirija a la ultima etapa que realizó alguna tarea.
